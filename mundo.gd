5195ff815e885026b4d6f9ac38fffdf2b2d1c96c
q <@>
2025-02-23 05:42:05 +0100

    docs: Añadidas licencias Creative Commons y GPL

--- mundo.gd
+++ mundo.gd
@@ -1,3 +1,19 @@
+# Este archivo es parte de LiCOpS.
+#
+# LiCOpS es software libre: puede redistribuirlo y/o 
+# modificarlo bajo los términos de la Licencia Pública General de GNU 
+# publicada por la Free Software Foundation, ya sea la versión 3 
+# de la Licencia, o (a su elección) cualquier versión posterior.
+#
+# LiCOpS se distribuye SIN GARANTÍA ALGUNA; ni siquiera 
+# la garantía implícita de COMERCIALIZACIÓN o IDONEIDAD PARA UN 
+# PROPÓSITO PARTICULAR. 
+# Consulte la Licencia Pública General de GNU para más detalles.
+#
+# Debería haber recibido una copia de la Licencia Pública General 
+# de GNU junto con este programa. 
+# En caso contrario, consulte <http://www.gnu.org/licenses/>.
+
 extends Node2D
 
 
