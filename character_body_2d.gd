ecc4665e8c788bd9417937c5b1353f0c74c6246b
Brais Pais Mariño <brais.pais@udc.es>
2025-02-23 01:09:37 +0100

    feat: Añadidos e implementadas las interaciones con los 4 sabios y el jefe

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -10,7 +10,6 @@ var player = null
 func _ready():
 	$Area2D.connect("body_entered", _on_body_entered)
 	$Area2D.connect("body_exited", _on_body_exited)
-	dialog_box.visible = false  # Ocultar diálogo al inicio
 
 func _on_body_entered(body):
 	if body.is_in_group("player"):
@@ -21,21 +20,18 @@ func _on_body_exited(body):
 	if body.is_in_group("player"):
 		player_near = false
 		player = null
-		hide_dialog()
 
 func _input(event):
 	if player_near and event.is_action_pressed(interaction_key):
-		toggle_dialog()
-
-func toggle_dialog():
-	if dialog_box.visible:
-		hide_dialog()
-	else:
-		show_dialog()
-
-func show_dialog():
-	dialog_label.text = "Hola"  # Texto del diálogo
-	dialog_box.visible = true
-
-func hide_dialog():
-	dialog_box.visible = false
+		if get_parent().name == "printf":
+			DialogueManager.show_dialogue_balloon(load("res://dialogue.dialogue"),"printf_charla")
+		elif get_parent().name == "scanf":
+			DialogueManager.show_dialogue_balloon(load("res://dialogue.dialogue"),"scanf_charla")
+		elif get_parent().name == "if":
+			DialogueManager.show_dialogue_balloon(load("res://dialogue.dialogue"),"if_charla")	
+		elif get_parent().name == "for":
+			DialogueManager.show_dialogue_balloon(load("res://dialogue.dialogue"),"for_charla")
+		elif get_parent().name == "windows":
+			DialogueManager.show_dialogue_balloon(load("res://dialogue.dialogue"),"windows_charla")
+		elif get_parent().name == "lukario":
+			DialogueManager.show_dialogue_balloon(load("res://dialogue.dialogue"),"puerta")			
